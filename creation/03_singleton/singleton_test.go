7af536b2225692a603a97124b2279baea0b4babd
Edward <chenlulei@gmail.com>
2020-04-23 09:47:58 +0800

    finish Singleton pattern

--- creation/03_singleton/singleton_test.go
+++ creation/03_singleton/singleton_test.go
@@ -5,30 +5,58 @@ import (
 	"testing"
 )
 
-const parCount = 100
+const workerCount = 500
 
-func TestSingleton(t *testing.T) {
-	ins1 := GetInstance()
-	ins2 := GetInstance()
+func TestWorkerSingleton(t *testing.T) {
+	ins1 := GetWorkerInstance()
+	ins2 := GetWorkerInstance()
 	if ins1 != ins2 {
-		t.Fatal("instance is not equal")
+		t.Fatal("worker(instance) is not exactly the same")
 	}
 }
 
-func TestParallelSingleton(t *testing.T) {
+// 获取500次，Worker 是否总是同一个worker
+func TestParallelWorkerSingleton(t *testing.T) {
 	wg := sync.WaitGroup{}
-	wg.Add(parCount)
-	instances := [parCount]*Singleton{}
-	for i := 0; i < parCount; i++ {
+	wg.Add(workerCount)
+	instances := [workerCount]*Worker{}
+	for i := 0; i < workerCount; i++ {
 		go func(index int) {
-			instances[index] = GetInstance()
+			instances[index] = GetWorkerInstance()
 			wg.Done()
 		}(i)
 	}
 	wg.Wait()
-	for i := 1; i < parCount; i++ {
+	for i := 1; i < workerCount; i++ {
 		if instances[i] != instances[i-1] {
-			t.Fatal("instance is not equal")
+			t.Fatal("Worker instance is not equal")
+		}
+	}
+}
+
+func TestManagerSingleton(t *testing.T) {
+	ins1 := GetManagerInstance()
+	ins2 := GetManagerInstance()
+	if ins1 != ins2 {
+		t.Fatal("Manager(instance) is not exactly the same")
+	}
+}
+
+// 获取500次，Manager 是否总是同一个Manager
+func TestParallelManagerSingleton(t *testing.T) {
+	wg := sync.WaitGroup{}
+	wg.Add(workerCount)
+	instances := [workerCount]*Manager{}
+	for i := 0; i < workerCount; i++ {
+		go func(index int) {
+			instances[index] = GetManagerInstance()
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	for i := 1; i < workerCount; i++ {
+		if instances[i] != instances[i-1] {
+			t.Fatal("Manager instance is not exactly equal")
 		}
 	}
 }
