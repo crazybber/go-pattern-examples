911fc9d50ba3e4d2b5dd3a3da1f42fd7c34897a3
Edward <chenlulei@gmail.com>
2020-04-30 17:47:41 +0800

    finish proxy pattern

--- structure/09_proxy/proxy.go
+++ structure/09_proxy/proxy.go
@@ -1,30 +1,68 @@
 package proxy
 
-type Subject interface {
-	Do() string
+import "errors"
+
+////////////////////////////////
+//用一个代理注册公司的例子，正向代理
+////////////////////////////////
+
+//Nation 国籍
+type Nation int
+
+//nation catalog
+const (
+	CN Nation = iota
+	UK
+	JP
+)
+
+//IPioneer 一个创业者，要注册公司
+type IPioneer interface {
+	RegisterCompany(companyName string) (name, enterpriseNo string, err error)
+
+	Conndition() (money int, kind Nation)
 }
 
-type RealSubject struct{}
-
-func (RealSubject) Do() string {
-	return "real"
+//Pioneer 一个创业者
+type Pioneer struct {
+	AccountMoney int
+	NationKind   Nation
 }
 
-type Proxy struct {
-	real RealSubject
+//RegisterCompany 创业者要注册公司
+func (p Pioneer) RegisterCompany(companyName string) (name, enterpriseNo string, err error) {
+
+	return
 }
 
-func (p Proxy) Do() string {
-	var res string
+//Conndition 注册条件
+func (p Pioneer) Conndition() (money int, kind Nation) {
+	money = p.AccountMoney
+	kind = p.NationKind
+	return
+}
 
-	// 在调用真实对象之前的工作，检查缓存，判断权限，实例化真实对象等。。
-	res += "pre:"
+//RegistryProxyCompany 代注公司，帮用户注册，对用户来讲，可以当成工商局来看待.
+type RegistryProxyCompany struct {
+	p IPioneer
+}
 
-	// 调用真实对象
-	res += p.real.Do()
+//RegisterCompany 代表用户注册公司
+func (r RegistryProxyCompany) RegisterCompany(companyName string) (name, enterpriseNo string, err error) {
 
-	// 调用之后的操作，如缓存结果，对结果进行处理等。。
-	res += ":after"
+	//检查注册人的，资金,姓名，
+	money, nation := r.p.Conndition()
 
-	return res
+	if money < 10000 || nation != CN {
+		return "", "", errors.New("Condition not OK")
+	}
+	////////////////////////////////
+	///发送请求到工商局
+	////////////////////////////////
+
+	name = companyName
+	enterpriseNo = "abvdefe12450"
+
+	return name, enterpriseNo, err
+
 }
--- structure/09_proxy/proxy_test.go
+++ structure/09_proxy/proxy_test.go
@@ -1,14 +1,33 @@
 package proxy
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestProxy(t *testing.T) {
-	var sub Subject
-	sub = &Proxy{}
 
-	res := sub.Do()
+	proxyCompany := RegistryProxyCompany{p: Pioneer{10000, UK}}
 
-	if res != "pre:real:after" {
-		t.Fail()
+	//想注册一个叫fire company 的公司
+	name, no, err := proxyCompany.RegisterCompany("fire company")
+
+	if err != nil {
+		t.Log("something wrong:", err.Error())
+	} else {
+
+		t.Log("got company, name:", name, "company no:", no)
+
+	}
+
+	proxyCompany = RegistryProxyCompany{p: Pioneer{109999, CN}}
+
+	//想注册一个叫fire company 的公司
+	name, no, err = proxyCompany.RegisterCompany("water company")
+
+	if err != nil {
+		t.Log("something wrong:", err.Error())
+	} else {
+
+		t.Log("got company, name:", name, "company no:", no)
 	}
 }
