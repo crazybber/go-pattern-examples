b9a9dc740d2c752a157e8348a6996e85d7a83595
Edward <crazybber@outlook.com>
2020-05-01 00:16:49 +0800

    add rate_limiting

--- gomore/rate_limiting/rate_limiting_test.go
+++ gomore/rate_limiting/rate_limiting_test.go
@@ -60,46 +60,6 @@ func TestRateLimiting(t *testing.T) {
 		fmt.Println("request handled", req, time.Now())
 	}
 
-	rateLimiting()
+	//rateLimiting()
 
-}
-
-func rateLimiting() {
-
-	requests := make(chan int, 5)
-	for i := 1; i <= 5; i++ {
-		requests <- i
-	}
-	close(requests)
-
-	limiter := time.Tick(200 * time.Millisecond)
-
-	for req := range requests {
-		<-limiter
-		fmt.Println("request", req, time.Now())
-	}
-
-	//突发限流器
-	burstyLimiter := make(chan time.Time, 3)
-
-	for i := 0; i < 3; i++ {
-		burstyLimiter <- time.Now()
-	}
-
-	go func() {
-		for t := range time.Tick(200 * time.Millisecond) {
-			burstyLimiter <- t
-		}
-	}()
-
-	//请求队列
-	burstyRequests := make(chan int, 5)
-	for i := 1; i <= 5; i++ {
-		burstyRequests <- i
-	}
-	close(burstyRequests)
-	for req := range burstyRequests {
-		<-burstyLimiter
-		fmt.Println("request", req, time.Now())
-	}
 }
