6a72ca4b8ea75583d5801546d9eb600b1c0eb4aa
Edward <crazybber@outlook.com>
2020-05-05 22:43:54 +0800

    add code content for  state pattern

---
+++ behavior/10_state/state_clock.go
@@ -0,0 +1,48 @@
+// Package state is an example of the State Pattern.
+package state
+
+////////////////////////////////
+//闹装有两种状态，震铃状态，非震铃状态
+
+// MobileAlertStater provides a common interface for various states.
+type MobileAlertStater interface {
+	Alert() string
+}
+
+// MobileAlert implements an alert depending on its state.
+type MobileAlert struct {
+	state MobileAlertStater
+}
+
+// Alert returns a alert string
+func (a *MobileAlert) Alert() string {
+	return a.state.Alert()
+}
+
+// SetState changes state
+func (a *MobileAlert) SetState(state MobileAlertStater) {
+	a.state = state
+}
+
+// NewMobileAlert is the MobileAlert constructor.
+func NewMobileAlert() *MobileAlert {
+	return &MobileAlert{state: &MobileAlertVibration{}}
+}
+
+// MobileAlertVibration implements vibration alert
+type MobileAlertVibration struct {
+}
+
+// Alert returns a alert string
+func (a *MobileAlertVibration) Alert() string {
+	return "vibrating humming ... vibrating humming...vibrating humming..."
+}
+
+// MobileAlertSong implements beep alert
+type MobileAlertSong struct {
+}
+
+// Alert returns a alert string
+func (a *MobileAlertSong) Alert() string {
+	return "sun rise ,get up ,get up get up..."
+}
--- behavior/10_state/state_test.go
+++ behavior/10_state/state_test.go
@@ -1,6 +1,27 @@
 package state
 
-func ExampleWeek() {
+import "testing"
+
+func TestState(t *testing.T) {
+
+	expect := "vibrating humming ... vibrating humming...vibrating humming..." +
+		"vibrating humming ... vibrating humming...vibrating humming..." +
+		"sun rise ,get up ,get up get up..."
+
+	mobile := NewMobileAlert()
+
+	result := mobile.Alert()
+	result += mobile.Alert()
+
+	mobile.SetState(&MobileAlertSong{})
+
+	result += mobile.Alert()
+
+	if result != expect {
+		t.Errorf("Expect result to equal %s, but %s.\n", expect, result)
+	}
+}
+func TestWeeks(t *testing.T) {
 	ctx := NewDayContext()
 	todayAndNext := func() {
 		ctx.Today()
