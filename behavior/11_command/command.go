fd36971c2124fc29d0a094e52c9966969a2dd07c
Edward <crazybber@outlook.com>
2020-05-05 22:03:44 +0800

    finish cmd pattern

--- behavior/11_command/command.go
+++ behavior/11_command/command.go
@@ -2,64 +2,75 @@ package command
 
 import "fmt"
 
-type Command interface {
+////////////////////////////////
+//这里使用军训的例子，使用队列向左转，向右转，向后转的口令
+//命令发出者是教官,命令执行者是队列
+
+//ICommand 命令接口，队列要进行响应
+type ICommand interface {
 	Execute()
 }
 
-type StartCommand struct {
-	mb *MotherBoard
+//Troop 队列
+type Troop struct{ name string }
+
+//Execute cmd
+func (t *Troop) Execute() {
+	fmt.Println("cmd had been executed by", t.name)
 }
 
-func NewStartCommand(mb *MotherBoard) *StartCommand {
-	return &StartCommand{
-		mb: mb,
+//TurnLeftCommand 向左转
+type TurnLeftCommand struct {
+	//可以携带参数,每个命令有一个接收者，去执行
+	receiver ICommand
+}
+
+//Execute 执行向左转
+func (t *TurnLeftCommand) Execute() {
+	fmt.Print("Troop Turn Left\n")
+	t.receiver.Execute()
+}
+
+//TurnRightCommand 向右转
+type TurnRightCommand struct {
+	//可以携带参数
+	receiver ICommand
+}
+
+//Execute 执行向右转
+func (t *TurnRightCommand) Execute() {
+	fmt.Print("Troop Turn Right\n")
+	t.receiver.Execute()
+}
+
+//TurnBackCommand 向后转
+type TurnBackCommand struct {
+	//可以携带参数
+	holdTime int //保持时间
+	receiver ICommand
+}
+
+//Execute 执行向后转
+func (t *TurnBackCommand) Execute() {
+	fmt.Print("Troop Turn Back\n")
+	t.receiver.Execute()
+}
+
+// Instructor 教官
+type Instructor struct {
+	commands []ICommand
+}
+
+//AddCommand 教官喊口令一般都是一连串
+func (i *Instructor) AddCommand(c ICommand) {
+	i.commands = append(i.commands, c)
+}
+
+//Execute  教官的Execute是发出命令
+func (i *Instructor) Execute() {
+
+	for _, cmd := range i.commands {
+		cmd.Execute()
 	}
-}
 
-func (c *StartCommand) Execute() {
-	c.mb.Start()
-}
-
-type RebootCommand struct {
-	mb *MotherBoard
-}
-
-func NewRebootCommand(mb *MotherBoard) *RebootCommand {
-	return &RebootCommand{
-		mb: mb,
-	}
-}
-
-func (c *RebootCommand) Execute() {
-	c.mb.Reboot()
-}
-
-type MotherBoard struct{}
-
-func (*MotherBoard) Start() {
-	fmt.Print("system starting\n")
-}
-
-func (*MotherBoard) Reboot() {
-	fmt.Print("system rebooting\n")
-}
-
-type Box struct {
-	buttion1 Command
-	buttion2 Command
-}
-
-func NewBox(buttion1, buttion2 Command) *Box {
-	return &Box{
-		buttion1: buttion1,
-		buttion2: buttion2,
-	}
-}
-
-func (b *Box) PressButtion1() {
-	b.buttion1.Execute()
-}
-
-func (b *Box) PressButtion2() {
-	b.buttion2.Execute()
 }
--- behavior/11_command/command_test.go
+++ behavior/11_command/command_test.go
@@ -1,20 +1,34 @@
 package command
 
-func ExampleCommand() {
-	mb := &MotherBoard{}
-	startCommand := NewStartCommand(mb)
-	rebootCommand := NewRebootCommand(mb)
+import "testing"
 
-	box1 := NewBox(startCommand, rebootCommand)
-	box1.PressButtion1()
-	box1.PressButtion2()
+func TestTroopCommand(t *testing.T) {
 
-	box2 := NewBox(rebootCommand, startCommand)
-	box2.PressButtion1()
-	box2.PressButtion2()
-	// Output:
-	// system starting
-	// system rebooting
-	// system rebooting
-	// system starting
+	//教官
+	in := Instructor{}
+
+	//受训队伍1
+	tr1 := &Troop{name: "girl team"}
+	turnLeft := &TurnLeftCommand{receiver: tr1}
+
+	//受训队伍2
+	tr2 := &Troop{name: "boy team"}
+
+	turnRight := &TurnLeftCommand{receiver: tr2}
+
+	//准备命令发给不同的队伍
+	in.AddCommand(turnRight)
+	in.AddCommand(turnLeft)
+
+	turnback := &TurnBackCommand{receiver: tr2}
+
+	in.AddCommand(turnback)
+
+	turnback = &TurnBackCommand{receiver: tr1}
+
+	in.AddCommand(turnback)
+
+	//开始发布指令
+	in.Execute()
+
 }
