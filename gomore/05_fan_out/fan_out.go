fe3617814c042f9af7394e6158445ade5b919f86
Edward <chenlulei@gmail.com>
2020-05-07 15:08:32 +0800

    update code content for fan-out pattern

--- gomore/05_fan_out/fan_out.go
+++ gomore/05_fan_out/fan_out.go
@@ -1,131 +1,132 @@
 package fanout
 
-import (
-	"context"
-	"sync"
-	"sync/atomic"
+// Split 重复分发数据为多份
+// Split a channel into n channels that receive messages in a round-robin fashion.
+func Split(ch <-chan int, n int) []chan int {
 
-	"go.uber.org/zap"
-)
+	//get a queue of chan
+	cs := make([]chan int, n)
 
-var (
-	log, _ = zap.NewDevelopment()
-)
-
-const (
-	MaxWorkers      = 16
-	MaxQueueSize    = 512
-	MasterQueueSize = MaxQueueSize * MaxWorkers
-)
-
-type Pipeline struct {
-	workers map[int]*worker
-	chain   chan interface{}
-}
-
-func (p *Pipeline) Start(ctx context.Context) {
-	go func(pipe *Pipeline) {
-		for {
-			expectationWorkers := len(pipe.chain) % MaxWorkers
-			if expectationWorkers >= MaxWorkers {
-				expectationWorkers = 0
+	// Distributes the work in a round robin fashion among the stated number
+	// of channels until the main channel has been closed. In that case, close
+	// all channels and return.
+	distributeToChannels := func(ch <-chan int, cs []chan int) {
+		// Close every channel when the execution ends.
+		defer func(cs []chan int) {
+			for _, c := range cs {
+				close(c)
 			}
+		}(cs)
+
+		//this version will block
+		for {
+			//get a target from ch
 			select {
-			case <-ctx.Done():
-				return
-			case val, ok := <-pipe.chain:
+			case val, ok := <-ch:
 				if !ok {
 					return
 				}
-				go pipe.workers[expectationWorkers].stream(val)
+				//send value to all channels
+				for _, c := range cs {
+					c <- val
+				}
 			}
-		}
-	}(p)
-}
-
-func (p *Pipeline) Dispatch(msg interface{}) {
-	p.chain <- msg
-}
-
-type DispatcherBuilder func() Dispatcher
-
-func NewPipeline(d DispatcherBuilder, idle uint32, debug bool) *Pipeline {
-	ch := make(chan interface{}, MasterQueueSize)
-	wk := make(map[int]*worker)
-	for i := 0; i < MaxWorkers; i++ {
-		wk[i] = &worker{
-			index:      uint32(i + 1),
-			chain:      make(chan interface{}, MaxQueueSize),
-			mutex:      new(sync.Mutex),
-			debug:      debug,
-			idle:       idle,
-			Dispatcher: d(),
 		}
 	}
-	return &Pipeline{workers: wk, chain: ch}
+
+	// a worker to distribute message
+	go distributeToChannels(ch, cs)
+
+	return cs
 }
 
-type Dispatcher interface {
-	Before(context.Context) error
-	After() error
-	Process(interface{}) error
-}
+// Split2  多工作者重复分发
+// Split2 a channel into n channels that receive messages in a round-robin fashion.
+func Split2(ch <-chan int, n int) []<-chan int {
 
-type worker struct {
-	index   uint32
-	mutex   *sync.Mutex
-	running bool
-	chain   chan interface{}
-	debug   bool
-	idle    uint32
-	Dispatcher
-}
+	cs := make([]chan int, n)
 
-func (c *worker) stream(val interface{}) {
-	c.chain <- val
-	if !c.running {
-		c.mutex.Lock()
-		c.running = true
-		ctx, cancel := context.WithCancel(context.Background())
-		defer func(w *worker, cancel context.CancelFunc) {
-			if w.debug {
-				log.Info("Worker leaving", zap.Any("index", w.index), zap.Any("idle", w.idle))
+	//this version will block
+	for {
+		//get a target from ch
+		select {
+		case val, ok := <-ch:
+			if !ok {
+				return cs
 			}
-			err := c.After()
-			if err != nil {
-				log.Error("can not finish track issue", zap.Error(err))
-			}
-			cancel()
-			w.mutex.Unlock()
-			w.running = false
-		}(c, cancel)
-		err := c.Before(ctx)
+			//send value to all channels
+			for _, c := range cs {
+				go func(tmpV int) {
+					c <- tmpV
+				}(val)
 
-		if err != nil {
-			log.Error("can not start worker", zap.Error(err))
+			}
 		}
-		var idle uint32 = 0
+	}
+
+	return cs
+}
+
+//Split3  随机分发到不同的目的地
+//Split3 a channel into n channels that receive messages in a round-robin fashion.
+func Split3(ch <-chan int, n int) []<-chan int {
+
+	cs := make([]chan int, n)
+
+	// Distributes the work in a round robin fashion among the stated number
+	// of channels until the main channel has been closed. In that case, close
+	// all channels and return.
+	distributeToChannels := func(ch <-chan int, cs []chan<- int) {
+		// Close every channel when the execution ends.
+		defer func(cs []chan<- int) {
+			for _, c := range cs {
+				close(c)
+			}
+		}(cs)
+
 		for {
-			select {
-			case msg := <-c.chain:
-				atomic.StoreUint32(&idle, 0)
-				if msg != nil {
-					err := c.Process(msg)
-					if err != nil {
-						log.Error("can not process message",
-							zap.Any("msg", &msg),
-							zap.Error(err),
-						)
-					}
-				}
-			default:
-				atomic.AddUint32(&idle, 1)
-				if i := atomic.LoadUint32(&idle); i > 0 {
-					if i > c.idle {
+			for _, c := range cs {
+				select {
+				case val, ok := <-ch:
+					if !ok {
 						return
 					}
+
+					c <- val
 				}
 			}
 		}
 	}
+
+	go distributeToChannels(ch, cs)
+
+	return cs
+}
+
+//The first stage, gen, is a function that converts a list of integers to a channel that emits the integers in the list.
+// The gen function starts a goroutine that sends the integers on the channel and closes the channel when all
+// the values have been sent:
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			out <- n
+		}
+	}()
+	return out
+}
+
+//The second stage, sq, receives integers from a channel and returns a channel that emits the square of
+// each received integer. After the inbound channel is closed and this stage has sent all the values downstream,
+// it closes the outbound channel:
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
 }
--- gomore/05_fan_out/fan_out_test.go
+++ gomore/05_fan_out/fan_out_test.go
@@ -1,1 +1,64 @@
 package fanout
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestMultiFanOutNumbersSeq(T *testing.T) {
+
+	//一路输入源
+	dataStreams := []int{13, 44, 56, 99, 9, 45, 67, 90, 78, 23}
+	//generator integer stream
+	inputChan := gen(dataStreams...)
+
+	// transfer to
+	ch := sq(inputChan)
+
+	//	split it to 3 channel
+	// 重复分发
+	outArray := Split(ch, 3)
+
+	var wg sync.WaitGroup
+	wg.Add(len(outArray))
+	for i := 0; i < len(outArray); i++ {
+
+		go func(in <-chan int, index int) {
+			sum := 0
+			for item := range in {
+				sum += item
+			}
+			fmt.Println("worker:", index, sum)
+
+			wg.Done()
+		}(outArray[i], i)
+	}
+	wg.Wait()
+}
+
+func TestManualFanOutNumbersSeq(T *testing.T) {
+
+	//一路输入源
+	dataStreams := []int{13, 44, 56, 99, 9, 45, 67, 90, 78, 23}
+	// generate the common channel with inputs
+	inputChan1 := gen(dataStreams...)
+	inputChan2 := gen(dataStreams...)
+
+	//Manual Fan-out to 2 Go-routine
+	c1 := sq(inputChan1)
+	c2 := sq(inputChan2)
+
+	fmt.Print("c1 queue: ")
+	for n := range c1 {
+		fmt.Print(n, " ")
+	}
+	fmt.Println()
+
+	fmt.Print("c2 queue: ")
+	for n := range c2 {
+		fmt.Print(n, " ")
+	}
+	fmt.Println()
+
+}
