04f7a4011323bb3d7c61c6641940813afaa3b095
Edward <chenlulei@gmail.com>
2020-05-08 19:34:33 +0800

    add a parallelism pattern

---
+++ gomore/09_parallelism/parallelism_test.go
@@ -0,0 +1,26 @@
+package parallelism
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"testing"
+)
+
+func TestParallelism(t *testing.T) {
+	// Calculate the MD5 sum of all files under the specified directory,
+	// then print the results sorted by path name.
+	m, err := MD5All(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var paths []string
+	for path := range m {
+		paths = append(paths, path)
+	}
+	sort.Strings(paths)
+	for _, path := range paths {
+		fmt.Printf("%x  %s\n", m[path], path)
+	}
+}
